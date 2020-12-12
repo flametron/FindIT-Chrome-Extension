71daaf18c8307bd1ef7c0ab84f8c92b86d1105cb
Sayan Kundu <sayanisflame@gmail.com>
2020-12-13 02:57:32 +0530

    Done

--- Popup.js
+++ Popup.js
@@ -1,14 +1,47 @@
 var storetabs = {};
 var hasaudiodir = [];
+var multipletitles = [];
+
+
+function addTolist(tab, index){
+    if(tab.title+"" in storetabs || multipletitles.includes(tab.title+"") ){
+        // if(tab.title in storetabs && !(multipletitles.includes(tab.title))){
+        //     chrome.tabs.get(parseInt(storetabs[tab.title]), function(tab3){
+        //         if(tab3.url == tab.url){
+        //             return;
+        //         }
+        // })
+        // }
+        // !TODO: not intended behaviour
+
+        if(multipletitles.includes(tab.title+"")){
+            //it already has 2 tabs
+            storetabs[tab.title+"("+tab.url+")"]=tab.id;
+        }
+        else{
+            var temptitle = tab.title+"";
+            var id = storetabs[temptitle];
+            delete storetabs[temptitle];
+            // Let's add the old one back
+            chrome.tabs.get(parseInt(id), function(tab2){
+                storetabs[temptitle+"("+tab2.url+")"]=id;
+            })
+            storetabs[temptitle+"("+tab.url+")"]=tab.id;
+            multipletitles.push(temptitle);
+        }
+        // console.log(multipletitles);
+    }
+    else{
+        storetabs[tab.title+""]=tab.id;
+    }
+    if(tab.audible){
+        hasaudiodir.push(tab.id);
+    }
+    }
 
 function startingCheck(){
     chrome.tabs.query({ currentWindow:true }, tabs => {
-        tabs.forEach( function(tab, index){
-            storetabs[tab.title+""]=tab.id;
-            if(tab.audible){
-                hasaudiodir.push(tab.id);
-            }
-            }
+        tabs.forEach( addTolist
         );
     });
 }
@@ -24,7 +57,7 @@ function addChild(ul,k,id){
     var listItem = document.createElement("li");
     var button = document.createElement("Button");
     button.setAttribute('class','foundlist');
-    button.textContent = k + " - "+ id;
+    button.textContent = k;
     button.setAttribute('tab-id',id);
     button.onclick = function(event) {
         goToTab(id);
@@ -47,10 +80,18 @@ function runParsing(){
         }
     }
     var ul = document.querySelector("#ResultList");
-    ul.innerHTML='';
-    for(var k in found){
-        addChild(ul,k,found[k]);
-    }
+    p = document.getElementById('Results');
+    
+    p.classList.add('hide')
+    setTimeout(function() { 
+        ul.innerHTML='';
+        for(var k in found){
+            addChild(ul,k,found[k]);
+        }
+    }, 50);
+    setTimeout(function() { 
+        p.classList.remove('hide')
+    }, 50);
 }
 
 function setListners(){
